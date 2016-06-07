33c1b3a198a0b0967e4e05804a2cb342c1799fa8
Alessandro Ursino <killanaca@gmail.com>
2016-06-07 15:02:35 +0200

    feat(postinstall): add postinstall code

--- dist/postinstall.js
+++ dist/postinstall.js
@@ -6,25 +6,35 @@ Object.defineProperty(exports, "__esModule", {
 
 let postinstall = (() => {
 	var ref = _asyncToGenerator(function* () {
-		const cwd = _path2.default.resolve();
+		// this is the path of the package: ..../node_modules/deployment-tools
+		const packagePath = process.cwd();
+		const src = _path2.default.join(packagePath, 'dist');
+		const projectPath = _path2.default.join(packagePath, '../../');
+		const destPath = _path2.default.join(projectPath, 'tools');
 		// first delete folder
-		const destFolder = _path2.default.join(process.cwd(), 'tools');
-		console.log('cwd', cwd, 'process.cwd()', process.cwd(), 'destFolder', destFolder);
-		debug(`try to delete folder(s) ${ destFolder }`);
-		// await del(destFolder, { dot: true });
+		yield (0, _del2.default)(destPath, { dot: true, dryRun: true });
+		console.log('src', src, 'projectPath', projectPath, 'destPath', destPath);
 		// then copy src into projects tools folder
-		// ncp(source, destFolder, function (err) {
-		// 	if (err) {
-		// 		return console.error(err);
-		// 	}
-		// 	console.log('done!');
-		// });
+		(0, _ncp2.default)(src, destPath, function (err) {
+			if (err) {
+				return console.error(err);
+			}
+			return console.log('done!');
+		});
 	});
 
 	return function postinstall() {
 		return ref.apply(this, arguments);
 	};
 })();
+
+var _ncp = require('ncp');
+
+var _ncp2 = _interopRequireDefault(_ncp);
+
+var _del = require('del');
+
+var _del2 = _interopRequireDefault(_del);
 
 var _path = require('path');
 
@@ -40,11 +50,5 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          * LICENSE.txt file in the root directory of this source tree.
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          *
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          */
-
-// import ncp from 'ncp';
-// import del from 'del';
-
-
-const debug = require('debug')('dt');
 
 exports.default = postinstall;
